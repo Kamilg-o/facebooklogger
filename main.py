47647bce6be6a29553b7d995664edac7d2abf2cd
Kamilg-o <kamilgrajko@interia.pl>
2021-06-13 00:53:36 +0200

    bypassing cookies notifications and add log in

--- main.py
+++ main.py
@@ -15,3 +15,12 @@ time.sleep(2)
 # bypassing the acceptance of cookies
 cookie_button = driver.find_element_by_xpath("//button[text()='Akceptuj wszystkie']")
 cookie_button.click()
+
+# log in
+username = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='email']")))
+password = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='pass']")))
+
+username.clear()
+username.send_keys('username')
+password.clear()
+password.send_keys('password')
